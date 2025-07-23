6f69ffb6df9611a22363eca8a8c364e6069f0082
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-07-23 22:01:22 +0530

    Create local_variables_using_functions.java
    To implement How local variables work using functions

---
+++ local_variables_using_functions.java
@@ -0,0 +1,10 @@
+class Test{
+    public static void main(String [] args){
+        fun();
+        fun();
+    }
+    public static void fun(){
+        int x = 5, y = 10;
+        System.out.println(x+y);
+    }
+}
