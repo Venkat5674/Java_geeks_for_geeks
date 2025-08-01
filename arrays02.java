17e036c35f4f087c222847508648ea09cc7f1c5e
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-01 10:31:19 +0530

    Create arrays02.java

---
+++ arrays02.java
@@ -0,0 +1,9 @@
+class arrays {
+    public static void main(String[] args) {
+        int [] a = {10, 20, 30, 40, 50}; // Declaration and initialization of an array
+        System.out.print("Array elements: "); // Print the elements of the array
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
