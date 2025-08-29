4a3130754e7b3cda244fb502847e31d3e7523093
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-29 11:12:53 +0530

    Add StringUpperLowerCase.java for case conversion

---
+++ StringUpperLowerCase.java
@@ -0,0 +1,11 @@
+//String Uppercase and Lower Case()
+class String1{
+    public static void main(String [] args){
+        String str1 = "VENKATESH PAMUDURTI";
+        String str2 = "venkatesh pamudurti";
+       
+        System.out.println(str2.toLowerCase());
+        System.out.println(str2.toUpperCase());
+       
+    }
+}
