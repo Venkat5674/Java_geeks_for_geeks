8f9b46a4c62bdc6a6e72266fbcc7d29da01bbe47
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-29 10:49:39 +0530

    Add String contains and equals demonstration

---
+++ String01.java
@@ -0,0 +1,10 @@
+//String Contains, Equals 
+class String1{
+    public static void main(String [] args){
+        String str1 = "Venkatesh";
+        String str2 = "Venkatesh";
+        String str3 = "Venkat";
+        System.out.println(str1.contains(str3));
+        System.out.println(str1.equals(str2));
+    }
+}
