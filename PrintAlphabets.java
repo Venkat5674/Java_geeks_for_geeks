c24b178465ea8814ed1045e5cc345312388086a2
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-29 11:38:28 +0530

    Add alphabets function to print characters
    Implement function to print alphabets from c1 to c2.

---
+++ PrintAlphabets.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+// function should print the alphabets
+// from c1 to c2 (space separated)
+// No need to print the new line
+// The new line will be printed by the
+// driver's code.
+class Solution {
+    public static void alphabets(char c1, char c2) {
+        // code here
+        for(char ch = c1; ch <=c2; ch++){
+            System.out.print(ch + " ");
+        }
+    }
+}
