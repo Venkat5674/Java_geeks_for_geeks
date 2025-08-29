1643efb777cbf7b8c8a42162bb35cc9494245871
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-29 11:08:25 +0530

    Add StringequalsIngoreCase class for case-insensitive comparison

---
+++ StringequalsIngoreCase.java
@@ -0,0 +1,10 @@
+//String equalsIngoreCase()
+class StringequalsIngoreCase{
+    public static void main(String [] args){
+        String str1 = "Venkatesh";
+        String str2 = "vENkatesH";
+       
+        System.out.println(str1.equalsIgnoreCase(str2));
+       
+    }
+}
