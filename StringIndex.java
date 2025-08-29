3b912ec7d8309e65a869f7ee0000afabd0669055
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-29 11:00:30 +0530

    Create StringIndex class with indexOf example
    Implement StringIndex class to demonstrate indexOf method.

---
+++ StringIndex.java
@@ -0,0 +1,10 @@
+//String IndexOf()
+class StringIndex{
+    public static void main(String [] args){
+        String str1 = "VenkateshVenkatesh";
+        String str2 = "Venkat";
+       
+        System.out.println(str1.indexOf(str2, 1));
+       
+    }
+}
