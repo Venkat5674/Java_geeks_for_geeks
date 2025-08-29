39aea32849ddd41c2a30c2745f2a41f6c446f168
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-29 10:18:17 +0530

    Add String1 class to demonstrate string methods

---
+++ String1.java
@@ -0,0 +1,10 @@
+class String1{
+    public static void main(String [] args){
+        String str = "Venkatesh";
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.substring(0,6));
+        System.out.println(str.substring(6));
+        
+    }
+}
